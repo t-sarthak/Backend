f004baf9d224b6e9718a0c2b675b106ef6ac1a1c
t-sarthak <singhsarthaktomar@gmail.com>
2024-05-09 15:57:28 +0530

    initial project setup

---
+++ src/main/java/com/backend/api/vehicle/search/VehicleSearchApplication.java
@@ -0,0 +1,13 @@
+package com.backend.api.vehicle.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/api/vehicle/search/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.api.vehicle.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
